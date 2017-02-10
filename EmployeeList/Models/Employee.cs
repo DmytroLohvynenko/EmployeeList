f997c1a9b163f1c93bade7aeeb42a1cc7fa50cdd
Dmytro_Lohvynenko <Dmytro_Lohvynenko@epam.com>
2017-02-10 11:40:14 +0200

    Inital commit. Creating Employee model

---
+++ EmployeeList/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace EmployeeList.Models
+{
+    public class Employee
+    {
+        private enum JobType
+        {
+            CEO,
+            BusinessAnalyst
+        }
+        public string Name { get; set; }
+
+    }
+}
