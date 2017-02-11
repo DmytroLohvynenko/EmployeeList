c7687e1f2bc19bad58b1214e06e7689244ace617
Dmytro_Lohvynenko <Lohvynenko.Dmytro@gmail.com>
2017-02-11 14:55:02 +0200

    add service prototype that use  to get data

---
+++ EmployeeListOnAngular/Scripts/app/employeesControllers.js
@@ -0,0 +1,9 @@
+﻿var employeesCortrollers = angular.module('employeesControllers', []);
+
+employeesCortrollers.controller('employeeListController', ['$scope', '$http', function($scope, $http){
+    $http.get('all.json').success(function(data){
+        var employees = [];
+
+        $scope.employees = employees;
+    });
+}]);
---
+++ EmployeeListOnAngular/Scripts/app/remoteService.js
@@ -0,0 +1,8 @@
+﻿var employeesService = angular.module('employeesService', ['ngResource']);
+
+employeesService.factory('Employees', ['$resource',
+    function($resource){
+        return $resource('all.json', {}, {
+            query: {method: 'GET', params: { }, isArray: true }
+        });
+    }]);
