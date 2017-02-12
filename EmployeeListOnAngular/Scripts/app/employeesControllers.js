11440bbc81083f67538075e93164fb135962b27d
Dmytro_Lohvynenko <Lohvynenko.Dmytro@gmail.com>
2017-02-12 22:32:33 +0200

    add view redrawing after deleting / creating employee

--- EmployeeListOnAngular/Scripts/app/employeesControllers.js
+++ EmployeeListOnAngular/Scripts/app/employeesControllers.js
@@ -1,24 +1,28 @@
 ﻿var employeesControllers = angular.module('employeesControllers', []);
 
 employeesControllers.controller('employeeListController', ['$scope', 'Employees', 'Employee', 'Constants', function($scope, Employees, Employee, Constants){
-    var employees = [];
     
-    Employees.query(function(employeeList){
-        angular.forEach(employeeList, function(empl){
-            empl.jobTitleDescription = Constants.JobType[empl.jobTitle].name;
-            employees.push(empl);
-        });
+    var queryEmployees = function(){
+        var employees = [];    
+        Employees.query(function(employeeList){
+            angular.forEach(employeeList, function(empl){
+                empl.jobTitleDescription = Constants.JobType[empl.jobTitle].name;
+                employees.push(empl);
+            });
 
-        $scope.employees = employees;
-    });
+            $scope.employees = employees;
+        });
+    };
+
+    queryEmployees();    
 
     $scope.removeEmployee = function(employeeId){
         Employee.remove({id:employeeId});
-    }
-
+        queryEmployees();
+    };
 }]);
 
-employeesControllers.controller('employeeCreateController', ['$scope', 'Employees', 'Constants', function($scope, Employees, Constants){
+employeesControllers.controller('employeeCreateController', ['$scope', '$route', 'Employees', 'Constants', function($scope, $route, Employees, Constants){
     $scope.jobTypes = Constants.JobType;
     $scope.newEmployee = {};
     $scope.newEmployee.employeeId = 0;
@@ -26,5 +30,6 @@ employeesControllers.controller('employeeCreateController', ['$scope', 'Employee
     $scope.saveEmployee = function(){
         var employees = new Employees($scope.newEmployee);
         employees.$save();
+        $route.reload();
     };
 }]);
